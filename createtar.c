89db3d89812af6376a5adbfc0dadcabeec9cc697
caseydaly <cadaly@calpoly.edu>
2018-05-17 10:08:00 -0700

    written but not debugged

--- createtar.c
+++ createtar.c
@@ -21,7 +21,7 @@ int createArchive(int file, char *path, int verbose, int strict)
 	block *header = makeBlock();i
 	
 	struct stat fileStats;
-	if (fstat(file, &fileStats) < 0) {
+	if (stat(path, &fileStats) < 0) {
 		perror("error in stat buffering\n");
 		exit(-1);
 	}
@@ -39,12 +39,80 @@ int createArchive(int file, char *path, int verbose, int strict)
 	printf("%s\n", (char*) header->name);
 
 	/*write octal permissions into a buffer and then write to block*/
-	char mode[8];
-	int permissions = fileStats.st_mode & (S_IRWXU | S_IRWXG | S_IRWXO);
-	int size = snprintf(mode, 8, "%o", permissions);
-	strcpy((char*)header->mode, mode);
+	char modeBuff[8];
+	int mode = fileStats.st_mode & (S_IRWXU | S_IRWXG | S_IRWXO);
+	int size = snprintf(modeBuff, 8, "%o", mode);
+	strcpy((char*)header->mode, modeBuff);
 
+	/*write octal uid into buffer then write to block*/
+	char uidBuff[8];
+	int uid = fileStats.st_uid;
+	size = snprintf(uidBuff, 8, "%o", uid)
+	strcpy((char*)header->uid, uidBuff);
+
+	/*write octal gid """"" */
+	char gidBuff[8];
+	int gid = fileStats.st_gid;
+	size = snprintf(gidBuff, 8, "%o", gid);
+	strcpy((char*)header->gid, gidBuff);
+
+	/*to conform with return value of snprintf above, size is bytes written to the buff and file_size is the number of bytes in the file. write octal file size to block*/
+
+	char fileSizeBuff[12];
+	int fileSize = fileStats.st_size;
+	size = snprintf(fileSizeBuff, 12, "%o", fileSize);
+	strcpy((char*)header->size, fileSizeBuff);
 	
+	/*write octal mtime to block*/
+	char mtimeBuff[12]
+	int mtime = fileStats.st_mtime;
+	size = snprintf(mtimeBuff, 12, "%o", mtime);
+	strcpy((char*)header->mtime, mtimeBuff);
+
+	/*see chksum filled in at the end, depends on all other fields being completed*/
+	
+
+	/*check for file type and write corresponding ascii number to block*/
+
+	char type;
+	if (S_ISLNK(file_stats.st_mode)) {
+		type = '2';
+	} else if (S_ISREG(file_stats.st_mode)) {	
+		type = '0';
+	} else if(S_ISDIR(file_stats.st_mode)) {
+		type = '5';
+	}
+	header->typeflag = type;
+
+	/*if file is a symbolic link, get its name into buffer and put it in block*/
+	if (type == '2') {
+		char link[100];
+		int linkSize = readlink(path, link, 100);
+		strcpy((char*)header->linkname, link);
+	
+	}
+
+	/*write magic num and version, always "ustar" (null-terminated) and "00" respectively*/
+	strcpy((char*)header->magic, "ustar");
+	header->version[0] = '0';
+	header->version[1] = '0';
+
+	/*get user name into unameBuff and then write to block*/
+	char unameBuff[32];
+	struct passwd *userInfo = getpwuid(fileStats.st_uid);
+	strncpy(unameBuff, userInfo->pw_name, 31);
+	unameBuff[32] = '\0';
+	strcpy(header->uname, unameBuff);
+
+	/*get group name into gnameBuff and then write to block*/
+	char gnameBuff[32]; 
+        struct group *groupInfo = getgrgid(fileStats.st_gid);
+        strncpy(gnameBuff, groupInfo->gr_name, 31);
+	gnameBuff[32] = '\0';
+	strcpy(header->gname, gnameBuff);
+
+	/*wasn't sure what to do for devmajor and devminor yet... think they might just be all 0's everytime since I think they only have to do with block special files, which are unsupported in hw4, but not 100% sure*/
+
         return 0;
 }
 
